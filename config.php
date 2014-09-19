c06ef5be2e50d1a5b3b70ba3948b1c814baa92dd
Mohammad Farid <zackseen@gmail.com>
2014-09-20 02:00:46 +0800

    revert change
    revert change

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
 
 /* run the library */
+$dir = "inc";
+
 
 // Below is another solution you can use, but you must have all your files inside a folder of your application, 
 // you can then search a folder for all PHP files and include all these files in your application. 
 // This uses the PHP function glob() which will search for a pattern of file names and return them as an array. 
 // You can use this array to loop through the values and include the file, like the following code.
-foreach (glob(LIB_DIR."/*.inc") as $filename)
+foreach (glob($dir."/*.inc") as $filename)
 {
     include $filename;
 }

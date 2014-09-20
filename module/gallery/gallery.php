60a369542d8ceb68e52aa86203185ee159b2511a
Mohammad Farid <zackseen@gmail.com>
2014-09-21 00:55:55 +0800

    update

--- module/gallery/gallery.php
+++ module/gallery/gallery.php
@@ -1,12 +1,4 @@
 <?php
 
-<<<<<<< HEAD
 include 'view/gallery.php';
-=======
-<<<<<<< HEAD
-include 'view/gallery.php';
-=======
-include_once 'view/gallery.php';
->>>>>>> origin/master
->>>>>>> origin/master
 ?>

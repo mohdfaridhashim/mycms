38cc8794bdf58b12bbb596fddfb4178399cc8b40
Mohammad Farid <zackseen@gmail.com>
2014-09-21 02:38:47 +0800

    update sidebar
    update sidebar

--- index.php
+++ index.php
@@ -15,18 +15,9 @@
 			    </div>
 			    <div class="col-sm-3 sidebar">
 			      <!--Sidebar content-->
-			            <div class="thumbnail">
-				            <h4>About</h4>
-				            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-				        </div>
-				        <div role="navigation">
-			              <ul class="list-group">
-				            <li class="list-group-item active">Coffee</li>            
-				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
-				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
-				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
-				          </ul>
-			        	</div><!--/span-->
+			         <?php
+			      		url_routing(define_module_sidebar()); 
+			      	 ?>
 			    </div>
 
 		  </div>
---
+++ module/sidebar/view/sidebar.php
@@ -0,0 +1,12 @@
+ <div class="thumbnail">
+				            <h4>About</h4>
+				            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+				        </div>
+				        <div role="navigation">
+			              <ul class="list-group">
+				            <li class="list-group-item active">Coffee</li>            
+				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
+				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
+				            <a href="http://getbootstrap.com/examples/offcanvas/#" class="list-group-item">Link</a>
+				          </ul>
+			        	</div><!--/span-->

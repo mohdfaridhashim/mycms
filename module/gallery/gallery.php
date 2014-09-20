966b66a142374bd634b838410ceaae25fc475562
Mohammad Farid <zackseen@gmail.com>
2014-09-21 00:35:59 +0800

    Update

---
+++ module/gallery/gallery.php
@@ -0,0 +1,4 @@
+<?php
+
+include 'view/gallery.php';
+?>
---
+++ module/gallery/view/gallery.php
@@ -0,0 +1,54 @@
+<!-- Gallery-->
+			    <div>
+				    <h3>Gallery</h3>
+				    <hr>
+				</div>
+
+				<div>
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>
+
+						  <div class="col-xs-6 col-md-3">
+						    <a href="#" class="thumbnail">
+						      <img data-src="holder.js/100%x180" alt="...">
+						    </a>
+						  </div>

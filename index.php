40729733231f7172093328196b58e0f542acf663
Mohammad Farid <zackseen@gmail.com>
2014-09-21 17:59:24 +0800

    UPDATE
    added POST validity

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 			  	<div class="col-sm-9 main">
 			      <?php
 			      url_routing(define_module()); 
+			      
 			      ?>
 
 				</div>
--- module/content/content.php
+++ module/content/content.php
@@ -3,17 +3,13 @@
 
 
 
-	//if(user_permission('1') ==0 )
-	//{
+
 		url_routing(define_module_combine('1000'),TITLE."&Welcome to my humble Page.");
 		include_once 'view/content.php';
 		url_routing(define_module_combine('1100'));
 		url_routing(define_module_combine('1200'));
 		url_routing(define_module_combine('1100'));
 
-
-
-	//}
 	
 	
 

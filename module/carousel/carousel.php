084c35e4bbdc0ede1d985c458ab55ea307d546e5
mohd farid hashim <zackseen@gmail.com>
2014-10-07 09:15:35 +0800

    update

---
+++ module/carousel/carousel.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'one.php';
---
+++ module/carousel/view/one.php
@@ -0,0 +1,20 @@
+<div id="myCarousel" class="carousel slide">
+   <!-- Carousel indicators -->
+   <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" 
+         class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+   </ol>   
+   <!-- Carousel items -->
+   <div class="carousel-inner">
+      <div class="item active">
+         <img src="slide1.png" alt="First slide">
+      </div>
+      <div class="item">
+         <img src="slide2.png" alt="Second slide">
+      </div>
+      <div class="item">
+         <img src="slide3.png" alt="Third slide">
+      </div>
+   </div>
